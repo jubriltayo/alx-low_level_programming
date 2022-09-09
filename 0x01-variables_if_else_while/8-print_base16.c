bb6855ee282fc49caccecfe6acbec8712ebf7215
Tayo Jubril <jubriltayo@gmail.com>
2022-09-09 15:05:43 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints hexadecimal string
+ *
+ * Return: Always (0) on success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
