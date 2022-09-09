2a7ddbc0980e1229a9881027aef0ce0fe7a05c39
Tayo Jubril <jubriltayo@gmail.com>
2022-09-09 12:13:28 -0700

    print number 0 to 9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print numbers 0 - 9
+ * main - print numbers 0 to 9
  *
  * Return: Always (success)
  */
@@ -9,7 +9,7 @@ int main(void)
 {
 	char n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
 	}
