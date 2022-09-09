b8474c2e5e19397458ec7f8559324a7a33f043f4
Tayo Jubril <jubriltayo@gmail.com>
2022-09-09 12:20:45 -0700

    print number 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print number 0 to 9
+ *
+ * Return: Always (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
