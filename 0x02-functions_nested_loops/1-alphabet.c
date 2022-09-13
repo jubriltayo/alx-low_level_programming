49ea58ef4994515cce6af0af0a1f6a4e07380fda
Tayo Jubril <jubriltayo@gmail.com>
2022-09-13 07:54:36 -0700

    prints low case alphabets with _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Description: prints lowercase alphabets with _putchar
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c ='a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
