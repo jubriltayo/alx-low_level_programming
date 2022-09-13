beae6edaae9fafa566dc5fc3bf5b1b9b03b441d1
Tayo Jubril <jubriltayo@gmail.com>
2022-09-13 08:28:04 -0700

    return 1 if lowercase and 0 if otherwise

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ *
+ * Return: 1 is lowercase and 0 if otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
