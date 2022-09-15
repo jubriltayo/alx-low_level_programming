018920da619a57128cc88903151884170f48d269
Tayo Jubril <jubriltayo@gmail.com>
2022-09-15 15:25:22 -0700

    prints 0 to 14, ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,14 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = i; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-			_putchar('1');
-			_putchar(j % 10 + '0');
+				_putchar('1');
+				_putchar((j % 10) + '0');
 		}
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
