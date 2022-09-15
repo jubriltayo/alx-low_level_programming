4b65d52d9a7f46867f48c1f4d0003f71f4b17688
Tayo Jubril <jubriltayo@gmail.com>
2022-09-15 05:55:54 -0700

    created main.h, 0-main.c and readme.md

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test function that prints if an integer is positve or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	positive_or_negative(n);
+
+	return (0);
+}
