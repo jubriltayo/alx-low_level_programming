e2c00613ccdf59746f66f6882f217a953699ef7b
Tayo Jubril <jubriltayo@gmail.com>
2022-09-19 14:44:43 -0700

    create readme, main.h, and reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - pointer to update value
+ * @n: character as pointer
+ * Description: pointer to update value to 98
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
