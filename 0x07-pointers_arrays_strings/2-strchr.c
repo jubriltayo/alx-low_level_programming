b5356fcb2fb637421782a25d6592096376dbf2c4
Tayo Jubril <jubriltayo@gmail.com>
2022-09-26 07:22:47 -0700

    find character in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strchr - find character in string
+ * @s: source string
+ * @c: character to find
+ * Return: string from whence character was found
+ */
+
+char *_strchr(char *s, char c);
+{
+	int i = 0, j;
+
+	while (s[i])
+		i++;
+
+	for (j = 0; j <= i; j++)
+	{
+		if (c == s[j])
+		{
+			s += j;
+			return (s);
+		}
+	}
+
+	return ('\0');
+}
