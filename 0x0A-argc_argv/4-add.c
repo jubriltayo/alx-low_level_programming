358e7457f69b59c2bd53e786471e3a703ee513a8
Tayo Jubril <jubriltayo@gmail.com>
2022-09-30 09:02:33 -0700

    sum of numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - sum of numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 for success, 1 for error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int j, sum = 0;
+	char *e;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			e = argv[i];
+
+			for (j = 0; j < strlen(e); j++)
+			{
+				if (e[j] < 48 || e[j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(e);
+			e++;
+		}
+
+		printf("%d", sum);
+	}
+	else
+		printf("0\n");
+
+	return (0);
+}
