7258f419c0f95cfa16c1edae5a7c0359c5e4f0c2
Tayo Jubril <jubriltayo@gmail.com>
2022-09-30 07:28:16 -0700

    print all the arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print all arguments
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
+	return (0);
+}
