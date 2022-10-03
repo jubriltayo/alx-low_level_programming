3187835aeedc24665de325ef1b25c72003b9f004
Tayo Jubril <jubriltayo@gmail.com>
2022-10-03 15:08:22 -0700

    2-D array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,7 +28,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		a[i] = malloc(sizeof(int) * width)
+		a[i] = malloc(sizeof(int) * width);
 
 		if (a[i] == NULL)
 		{
