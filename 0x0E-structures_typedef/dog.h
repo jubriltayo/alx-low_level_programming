6964d93546adcba55f655982b06d685d2185aeb2
Tayo Jubril <jubriltayo@gmail.com>
2022-10-10 14:07:28 -0700

    dog details

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+  * struct dog - dog details
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner of dog
+  * Description: details of dog
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+  * my_dog - type definition (typedef) for dog
+  */
+typedef struct dog my_dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* MAIN_H */
