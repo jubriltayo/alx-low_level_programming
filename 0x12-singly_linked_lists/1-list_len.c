d2386aef2621fe2362b64ecf6c8fa70a01e427c0
Tayo Jubril <jubriltayo@gmail.com>
2022-10-20 10:18:41 -0700

    number of elements in a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - prints the number of elements in a list.
+ * @h: singly linked list.
+ *
+ * Return: number of elements in a list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t num;
+
+	num = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		num++;
+	}
+
+	return (num);
+}
