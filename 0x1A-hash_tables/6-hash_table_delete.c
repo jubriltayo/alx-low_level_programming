807d049f0f38820df8ffa1b6b413ee7ad61adf2e
Tayo Jubril <jubriltayo@gmail.com>
2023-04-13 12:04:09 +0100

    task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -13,21 +13,18 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int index = 0;
 	hash_node_t *temp = NULL;
 
-	if (ht)
+	while (index < ht->size)
 	{
-		for (index = 0; index < ht->size; index++)
+		while (ht->array[index])
 		{
-			while (ht->array[index])
-			{
-				temp = ht->array[index]->next;
-				free(ht->array[index]->key);
-				free(ht->array[index]->value);
-				free(ht->array[index]);
-				ht->array[index] = temp;
-			}
-			index++;
+			temp = ht->array[index]->next;
+			free(ht->array[index]->key);
+			free(ht->array[index]->value);
+			free(ht->array[index]);
+			ht->array[index] = temp;
 		}
-		free(ht->array);
-		free(ht);
+		index++;
 	}
+	free(ht->array);
+	free(ht);
 }
