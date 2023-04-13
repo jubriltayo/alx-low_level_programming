608c95701cf16e7f03be7e9875c5da4315cb568e
Tayo Jubril <jubriltayo@gmail.com>
2023-04-13 11:32:25 +0100

    task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -4,9 +4,8 @@
  * hash_table_print - prints keys and values in a hash table
  *
  * @ht: hash table
- * @key: key of an index
  *
- * Return: value for success, NULL for fail
+ * Return: void
  */
 
 void hash_table_print(const hash_table_t *ht)
