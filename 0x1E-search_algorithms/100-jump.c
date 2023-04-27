6f8c65ed75de85647110b2015231e221914268f9
Tayo Jubril <jubriltayo@gmail.com>
2023-04-27 22:51:46 +0100

    task 7

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,52 @@
+#include "search_algos.h"
+
+/**
+ * min - function to find the minimum value
+ * @x: first value
+ * @y: second value
+ *
+ * Return: the minimum value
+ */
+size_t min(size_t x, size_t y)
+{
+	return (x < y ? x : y);
+}
+
+/**
+ * jump_search - function to find the value in a sorted array
+ * @array: array to be searched
+ * @size: length of array
+ * @value: value to be searched
+ *
+ * Return: index of the value
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t prev = 0;
+	size_t step = 0;
+
+	if (!array || !size)
+		return (-1);
+
+	/* find block that may contain value */
+	while (array[min(step, size) - 1] < value)
+	{
+		printf("Value checked array[%ld] = [%d]\n", step, array[step]);
+		prev = step;
+		step = step + sqrt(size);
+
+		if (step > (size - 1))
+			break;
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n", prev, step);
+
+	/* linear search */
+	while ((prev <= min(step, size - 1)) && array[prev] <= value)
+	{
+		printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
+		prev++;
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,9 +2,12 @@
 #define __SEARCH_ALGOS_H__
 
 #include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t low, size_t high);
+int jump_search(int *array, size_t size, int value);
+size_t min(size_t x, size_t y);
 
 #endif /* __SEARCH_ALGOS_H__ */
