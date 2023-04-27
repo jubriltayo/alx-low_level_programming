1e96d3637f9236812bd261a61c6261317136b835
Tayo Jubril <jubriltayo@gmail.com>
2023-04-27 11:36:26 +0100

    task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,21 +1,58 @@
 #include "search_algos.h"
 
 /**
+ * print_array - prints an array
+ * @array: array to be searched
+ * @low: the first index to print
+ * @high: the last index to print
+ */
+void print_array(int *array, size_t low, size_t high)
+{
+	char *separator = NULL;
+
+	printf("Searching in array: ");
+	for (; low <= high; low++)
+	{
+		if (separator)
+			printf("%s%d", separator, array[low]);
+		else
+			printf("%d", array[low]);
+		separator = ", ";
+	}
+	printf("\n");
+}
+
+/**
  * binary_search - function to search for a value in an array
  * @array: array to be searched
  * @size: size of the array
- * @value: searched value
+ * @value: value to be searched
  *
  * Return: return index for success and -1 for otherwise
 */
-
-
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low = 0, mid, high = size - 1;
+	size_t low = 0, high = size - 1;
 
 	if (array == NULL)
 		return (-1);
+
+	print_array(array, low, high);
+	return (binary_helper(array, low, high, value));
+}
+
+/**
+ * binary_helper - helper function to search for a value in an array
+ * @array: array to be searched
+ * @low: start
+ * @high: end
+ * @value: value to be searched
+ *
+ * Return: return index for success and -1 for otherwise
+*/
+int binary_helper(int *array, size_t low, size_t high, int value)
+{
+	size_t mid;
 
 	while (low != high)
 	{
@@ -26,6 +63,7 @@ int binary_search(int *array, size_t size, int value)
 			high = mid - 1;
 		else if (value > array[mid])
 			low = mid + 1;
+		print_array(array, low, high);
 	}
 
 	/* edge cases */
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,4 +5,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t low, size_t high);
+int binary_helper(int *array, size_t low, size_t high, int value);
+
 #endif /* __SEARCH_ALGOS_H__ */
