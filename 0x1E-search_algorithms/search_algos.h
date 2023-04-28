baccf4f7490be6dca6b795ba10ec8e4eba35b3e3
Tayo Jubril <jubriltayo@gmail.com>
2023-04-28 21:26:06 +0100

    task 9

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,7 @@ int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t low, size_t high);
 int jump_search(int *array, size_t size, int value);
 size_t min(size_t x, size_t y);
+int interpolation_search(int *array, size_t size, int value);
+int interpol_helper(int *array, size_t low, size_t high, int value);
 
 #endif /* __SEARCH_ALGOS_H__ */
